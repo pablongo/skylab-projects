a5e4a108224c7e6deea33960fc3d9f9a238f21e8
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-28 18:43:59 +0200

    Test getOneUser and deleteUser

--- icomunicate-pau-gallardo/lib/controllers/userController.test.js
+++ icomunicate-pau-gallardo/lib/controllers/userController.test.js
@@ -1,5 +1,7 @@
 import User from '../../models/userModel';
-import { createNewUser } from './userController';
+import { createNewUser, getOneUser, deleteUser } from './userController';
+
+jest.mock('../../models/userModel');
 
 describe('Given a createNewUser function', () => {
   describe('When is invoked', () => {
@@ -13,11 +15,56 @@ describe('Given a createNewUser function', () => {
           body: { email: 'asda' },
         };
 
+        User.findOne.mockResolvedValue({});
         User.create.mockResolvedValue({});
 
         await createNewUser(req, res);
 
-        expect(res.send()).toHaveBeenCalled();
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a getOneUser function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.send should have been called', async () => {
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+        const req = {
+          query: { userId: '' },
+        };
+
+        User.findById.mockReturnValue({});
+
+        await getOneUser(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a deleteUser function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.send should have been called', async () => {
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+        const req = {
+          query: { userId: '' },
+        };
+
+        User.findByIdAndDelete.mockReturnValue({});
+
+        await deleteUser(req, res);
+
+        expect(res.send).toHaveBeenCalled();
       });
     });
   });

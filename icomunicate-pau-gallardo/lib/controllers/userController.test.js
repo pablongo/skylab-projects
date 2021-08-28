37bde96698abfda8d396b3e1dd45ef607540d2f9
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-28 17:37:32 +0200

    Test createNewUser

---
+++ icomunicate-pau-gallardo/lib/controllers/userController.test.js
@@ -0,0 +1,24 @@
+import User from '../../models/userModel';
+import { createNewUser } from './userController';
+
+describe('Given a createNewUser function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.send should have been called', async () => {
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+        const req = {
+          body: { email: 'asda' },
+        };
+
+        User.create.mockReturnedValue({});
+
+        await createNewUser(req, res);
+
+        expect(res.send()).toHaveBeenCalled();
+      });
+    });
+  });
+});

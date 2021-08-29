192555c3c1a6f836f49adf493038832a97600dae
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-29 13:48:29 +0200

    Test getOneUser rejection

--- icomunicate-pau-gallardo/lib/controllers/userController.test.js
+++ icomunicate-pau-gallardo/lib/controllers/userController.test.js
@@ -49,6 +49,23 @@ describe('Given a getOneUser function', () => {
         expect(res.send).toHaveBeenCalled();
       });
     });
+    describe('And rejectes', () => {
+      test('Then handleError should have been called', async () => {
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+        const req = {
+          query: { userId: '' },
+        };
+
+        User.findById.mockRejectedValue({});
+
+        await getOneUser(req, res);
+
+        expect(handleError).toHaveBeenCalled();
+      });
+    });
   });
 });
 

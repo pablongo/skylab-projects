6b73bdc8f4df839d20e72a37e84db76bcacb14bb
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-29 20:38:57 +0200

    Test updateDictionary

--- icomunicate-pau-gallardo/lib/controllers/dictionaryController.test.js
+++ icomunicate-pau-gallardo/lib/controllers/dictionaryController.test.js
@@ -1,6 +1,8 @@
 import Dictionary from '../../models/dictionaryModel';
 import handleError from '../../utils/handleError';
-import { createNewDictionary, getOneDictionary, deleteDictionary } from './dictionaryController';
+import {
+  createNewDictionary, getOneDictionary, deleteDictionary, updateDictionary,
+} from './dictionaryController';
 
 jest.mock('../../models/dictionaryModel');
 jest.mock('../../utils/handleError');
@@ -113,6 +115,45 @@ describe('Given a deleteDictionary function', () => {
 
         Dictionary.findByIdAndDelete.mockRejectedValue({});
         await deleteDictionary(req, res);
+
+        expect(handleError).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a updateDictionary function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then req.send should have been called', async () => {
+        const req = {
+          body: {},
+          query: { dictionaryId: '' },
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Dictionary.findByIdAndUpdate.mockResolvedValue({});
+        await updateDictionary(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then handleError should have been called', async () => {
+        const req = {
+          body: {},
+          query: { dictionaryId: '' },
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Dictionary.findByIdAndUpdate.mockRejectedValue({});
+        await updateDictionary(req, res);
 
         expect(handleError).toHaveBeenCalled();
       });

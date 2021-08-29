c68c3d3ce760d8bce16b9ec622cadcedf0a24a17
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-29 20:30:05 +0200

    Test getOneDictionary

--- icomunicate-pau-gallardo/lib/controllers/dictionaryController.test.js
+++ icomunicate-pau-gallardo/lib/controllers/dictionaryController.test.js
@@ -1,6 +1,6 @@
 import Dictionary from '../../models/dictionaryModel';
 import handleError from '../../utils/handleError';
-import { createNewDictionary } from './dictionaryController';
+import { createNewDictionary, getOneDictionary } from './dictionaryController';
 
 jest.mock('../../models/dictionaryModel');
 jest.mock('../../utils/handleError');
@@ -35,6 +35,45 @@ describe('Given a createNewDictionary function', () => {
 
         Dictionary.create.mockRejectedValue({});
         await createNewDictionary(req, res);
+
+        expect(handleError).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a getOneDictionary function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then req.send should have been called', async () => {
+        const req = {
+          body: {},
+          query: { dictionaryId: '' },
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Dictionary.findById.mockResolvedValue({});
+        await getOneDictionary(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then handleError should have been called', async () => {
+        const req = {
+          body: {},
+          query: { dictionaryId: '' },
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Dictionary.findById.mockRejectedValue({});
+        await getOneDictionary(req, res);
 
         expect(handleError).toHaveBeenCalled();
       });

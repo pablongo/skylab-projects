910d70356417377cb93768697a8ae013b587289b
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-29 18:45:54 +0200

    Test getPictogram and deletePictogram

--- icomunicate-pau-gallardo/lib/controllers/pictogramController.test.js
+++ icomunicate-pau-gallardo/lib/controllers/pictogramController.test.js
@@ -1,6 +1,6 @@
 import Pictogram from '../../models/pictogramModel';
 import handleError from '../../utils/handleError';
-import { createPictogram } from './pictogramController';
+import { createPictogram, getPictogram, deletePictogram } from './pictogramController';
 
 jest.mock('../../models/pictogramModel');
 jest.mock('../../utils/handleError');
@@ -35,6 +35,84 @@ describe('Given a createPictogram function', () => {
 
         Pictogram.create.mockRejectedValue({});
         await createPictogram(req, res);
+
+        expect(handleError).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a getPictogram function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.send should have been called', async () => {
+        const req = {
+          body: {},
+          query: {},
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Pictogram.findById.mockResolvedValue({});
+        await getPictogram(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then handleError should have been called', async () => {
+        const req = {
+          body: {},
+          query: {},
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Pictogram.findById.mockRejectedValue({});
+        await getPictogram(req, res);
+
+        expect(handleError).toHaveBeenCalled();
+      });
+    });
+  });
+});
+
+describe('Given a deletePictogram function', () => {
+  describe('When is invoked', () => {
+    describe('And resolves', () => {
+      test('Then res.send should have been called', async () => {
+        const req = {
+          body: {},
+          query: {},
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Pictogram.findByIdAndDelete.mockResolvedValue({});
+        await deletePictogram(req, res);
+
+        expect(res.send).toHaveBeenCalled();
+      });
+    });
+    describe('And rejectes', () => {
+      test('Then handleError should have been called', async () => {
+        const req = {
+          body: {},
+          query: {},
+        };
+        const res = {
+          send: jest.fn(),
+          status: jest.fn(),
+        };
+
+        Pictogram.findByIdAndDelete.mockRejectedValue({});
+        await deletePictogram(req, res);
 
         expect(handleError).toHaveBeenCalled();
       });

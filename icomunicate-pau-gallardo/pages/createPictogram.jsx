d80f30059660bcc31d8c303363f0b066e4257504
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-30 17:32:29 +0200

    Refactor createPictogram function

--- icomunicate-pau-gallardo/pages/createPictogram.jsx
+++ icomunicate-pau-gallardo/pages/createPictogram.jsx
@@ -13,15 +13,19 @@ export default function CreatePictogram() {
       </section>
       <section>
         <label htmlFor="browse-picture">
+          Browse Picture
           <input type="text" id="browse-picture" />
         </label>
         <label htmlFor="text">
-          <input type="text" id="browse-picture" />
+          Text
+          <input type="text" id="text" />
         </label>
         <label htmlFor="gramatic-type">
+          Gramar type
           <input type="text" id="gramatic-type" />
         </label>
         <label htmlFor="metadata">
+          Metadata
           <input type="text" id="metadata" />
         </label>
       </section>

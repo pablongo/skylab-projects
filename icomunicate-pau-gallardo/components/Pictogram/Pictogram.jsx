0a19ec3c9f0e02cefa4a2fd395c1c7c27da07058
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-30 17:47:26 +0200

    Create pictogram component

---
+++ icomunicate-pau-gallardo/components/Pictogram/Pictogram.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Pictogram() {
+  return (
+    <figure>
+      <img src="" alt="" />
+      <h2>Titulo del pictograma</h2>
+    </figure>
+  );
+}

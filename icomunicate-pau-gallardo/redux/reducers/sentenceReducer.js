d2b9f1cc9ef1502abf47e808a68e5f67a26fbf2c
Pau Gallardo <paugallrdocordon@gmail.com>
2021-08-31 18:04:50 +0200

    Create sentenceReducer
    - Create LOAD_PICTOGRAM case

--- icomunicate-pau-gallardo/redux/reducers/sentenceReducer.js
+++ icomunicate-pau-gallardo/redux/reducers/sentenceReducer.js
@@ -1,3 +1,13 @@
-export default function sentenceReducer(pictoArray = [], action) {
+import actionTypes from '../actions/actionTypes';
 
+export default function sentenceReducer(pictoArray = [], action) {
+  const newArray = pictoArray;
+  switch (action.type) {
+    case actionTypes.LOAD_PICTOGRAM:
+      break;
+
+    default:
+      break;
+  }
+  return newArray;
 }

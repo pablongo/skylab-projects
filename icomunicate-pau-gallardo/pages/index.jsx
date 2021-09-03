af466719e56c0c4ce1a7c9043ba7129d3d8b232e
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-03 20:43:26 +0200

    Add to dictionaryModel new property

--- icomunicate-pau-gallardo/pages/index.jsx
+++ icomunicate-pau-gallardo/pages/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSession } from 'next-auth/client';
 
 export default function Home() {
+  const [session] = useSession();
+  console.log(session);
   return (
     <>
       <h1>Esto es el Home</h1>

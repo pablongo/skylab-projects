b4650ef439a5bb43ac583f2686468e5cc7c06dee
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-05 12:19:39 +0200

    Refactor index

--- icomunicate-pau-gallardo/pages/index.jsx
+++ icomunicate-pau-gallardo/pages/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { useSession } from 'next-auth/client';
 
 export default function Home() {
-  const [session] = useSession();
   return (
     <>
       <h1>Esto es el Home</h1>

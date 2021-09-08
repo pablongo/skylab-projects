7bf6f41cf80d37a46290dbc2c359c807b2b3cc8e
Pau Gallardo <paugallrdocordon@gmail.com>
2021-09-08 13:15:48 +0200

    Refactor createPictogram
    - Add firebase config
    - Upload imgs and get link

--- icomunicate-pau-gallardo/components/createPictogram.jsx
+++ icomunicate-pau-gallardo/components/createPictogram.jsx
@@ -1,10 +1,12 @@
 import { useSession } from 'next-auth/client';
 import React, { useState } from 'react';
 import styles from '../styles/createPictogram.module.scss';
+import { storage } from '../firebase';
 
 export default function CreatePictogram() {
   const [session] = useSession();
   const [formData, setFormData] = useState();
+  const [image, setImage] = useState();
   const placeholder = 'https://via.placeholder.com/150';
 
   function handleFormData(event) {
@@ -14,7 +16,32 @@ export default function CreatePictogram() {
     });
   }
 
+  function handleChange(event) {
+    if (event.target.files[0]) {
+      setImage(event.target.files[0]);
+    }
+  }
+
+  function handleUpload() {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref('images')
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => url);
+      },
+    );
+  }
+
   async function submitHandler(data) {
+    handleUpload();
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
 
@@ -37,12 +64,12 @@ export default function CreatePictogram() {
     <section className={styles.create__page}>
       <img className={styles.create__page__img} src={placeholder} alt="" />
       <button className={styles.create__page__btn} type="submit" onClick={() => submitHandler(formData)}>
-        Save Changes
+        Upload
       </button>
       <form className={styles.create__page__form}>
         <label className={styles.create__page__form__label} htmlFor="image">
           <span className={styles.create__page__form__label__text}>Image URL</span>
-          <input className={styles.create__page__form__label__input} type="text" id="image" onChange={handleFormData} />
+          <input className={styles.create__page__form__label__input} type="file" id="image" onChange={handleChange} />
         </label>
         <label htmlFor="title">
           <span className={styles.create__page__form__label__text}>Pictogram title</span>
